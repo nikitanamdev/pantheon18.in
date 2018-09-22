68e508a163698b0f489ada256ff9b37bed6966a9
Manish Ranjan <mranjan1398@gmail.com>
2018-09-22 12:20:25 +0530

    team Schema

---
+++ api/models/teams.js
@@ -0,0 +1,37 @@
+const mongoose = require("mongoose");
+
+const teamSchema = new mongoose.Schema({
+    _id: mongoose.Schema.Types.ObjectId,
+    teamName: {
+        type: String,
+        required: true,
+        default: "",
+        unique: true
+    },
+    teamPassword: {
+        type: String,
+        required: true,
+        default: ""
+    },
+    teamMembers: [{
+        type: Number,
+    }],
+    points: {
+        type: Number,
+        default: 0
+    },
+    eventsRegistered: {
+        type: Number,
+        default: 1
+    },
+    buffer1: {
+        type: String,
+        default: ""
+    },
+    buffer2: {
+        type: String,
+        default: ""
+    }
+});
+
+module.exports = mongoose.model("teams", teamSchema);
