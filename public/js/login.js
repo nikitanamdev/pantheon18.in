70e0cfe2fd535faf7079dec68878351fbc587af5
Zemotacqy <mranjan1398@gmail.com>
2018-09-27 15:28:38 +0530

    direct login test

--- public/js/login.js
+++ public/js/login.js
@@ -1,8 +1,11 @@
+//const requrl = "https://www.pantheon18.in";
 const requrl = "https://www.pantheon18.in";
-
 $(document).ready(function () {
     $('#login').click((e) => {
         e.preventDefault();
+        if(validate_form()===false){
+            return;
+        }
         const email = $('#email').val().trim();
         const password = $('#pass').val();
         $.ajax({
@@ -28,3 +31,21 @@ $(document).ready(function () {
         });
     });
 });
+function validate_form() {
+    const mail = $("#email").val().trim();
+    const regmail = /^([\w-\.]+@([\w-]+\.)+[\w-]{2,4})?$/;
+    if(mail==="") {
+            alert("Enter Email");
+            return false;
+        }
+    if(!regmail.test(mail)){
+            $("#email").val("");
+            alert("Enter valid email address");
+            return false;
+        }
+    const password = $("#pass").val();
+    if(password.length<1) {
+            alert("Please Enter the password");
+            return false;
+        }
+}
